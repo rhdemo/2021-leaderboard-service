0210362c4bde17e64ffe2cd198a1777ce23f6fb8
Katia Aresti <karesti@redhat.com>
2021-03-01 23:28:06 +0100

    cleanup

--- src/main/java/com/redhat/InfinispanInit.java
+++ src/main/java/com/redhat/InfinispanInit.java
@@ -6,9 +6,7 @@ import io.quarkus.runtime.StartupEvent;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.commons.configuration.XMLStringConfiguration;
-import org.infinispan.counter.api.CounterManager;
 
-import javax.annotation.Priority;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -16,17 +14,13 @@ import javax.inject.Inject;
 @ApplicationScoped
 public class InfinispanInit {
 
-   public static final String INDEXED_PROTOBUF = "indexed-protobuf";
    @Inject
    RemoteCacheManager cacheManager;
-
-   @Inject
-   CounterManager counterManager;
 
    @ConfigProperty(name = "configureInfinispan")
    Boolean configureInfinispan;
 
-   void onStart(@Observes @Priority(value = 1) StartupEvent ev) {
+   void onStart(@Observes StartupEvent ev) {
       if(configureInfinispan) {
          cacheManager.administration().getOrCreateCache(PlayerScore.PLAYERS_SCORES, new XMLStringConfiguration(TEST_CACHE_PLAYER_CONFIG));
          cacheManager.administration()
